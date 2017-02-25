9a4926924bdc4fe77d2570b471acaee9f544abb2
ZnHoCn <zenudnju@gmail.com>
2017-02-26 01:04:49 +0800

    Updated: 2017-02-26

--- slog.sh
+++ slog.sh
@@ -4,7 +4,7 @@ cd $(cd `dirname $0`; pwd)
 touch date.log
 echo "$(date +%F) is OK" >> date.log
 touch data.txt
-cat /dev/urandom | base64 | head -c 1000 >> data.txt
+cat /dev/urandom | base64 | head -c 1000 > data.txt
 
 git pull
 git add .
